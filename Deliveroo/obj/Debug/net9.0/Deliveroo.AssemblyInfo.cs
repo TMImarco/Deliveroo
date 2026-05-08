8649cc9c22031458f6823752ed2ab78455248630
stefi !! <liastfy.ag@gmail.com>
2026-05-08 17:20:23 +0200

    Navbar fissata e modifica delle icone
    Ho fissato la navbar con la sticky-top nella header allinterno di layout.cshtml, di conseguenze ho dovuto modificare anche le icone "torna alla home", "cerca" degli articoli e "menu" per accedere all'admin così fanno parte della navbar e rimagono fisse (hanno la stessa grafica cosi sono fighe)
    Tutto questo non solo nell'utente, ma anche nell'admin (baci baci)

--- Deliveroo/obj/Debug/net9.0/Deliveroo.AssemblyInfo.cs
+++ Deliveroo/obj/Debug/net9.0/Deliveroo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Deliveroo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aa34859e3a557ca10bbdd534e1ae162e381651e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c4cc09570e2e9316519fe8e4c4b691261dd834")]
 [assembly: System.Reflection.AssemblyProductAttribute("Deliveroo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Deliveroo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
