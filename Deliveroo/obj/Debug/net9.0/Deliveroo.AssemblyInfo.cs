97fa786ac62cb16f000e3e743922cd2835e363f1
stefi !! <liastfy.ag@gmail.com>
2026-05-09 19:23:25 +0200

    Modifica della grafica
    Modificato la grafica del: Index.cshtml, _Layout.cshtml, Articoli.cshtml

--- Deliveroo/obj/Debug/net9.0/Deliveroo.AssemblyInfo.cs
+++ Deliveroo/obj/Debug/net9.0/Deliveroo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Deliveroo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8674dc93f969f58eaca5067b725e4bc0fd9a4528")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a13efece34ae573e245c26373e5c3cfa81ff880")]
 [assembly: System.Reflection.AssemblyProductAttribute("Deliveroo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Deliveroo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
